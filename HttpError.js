ef8251cebef8729a40339bd146b39f650ebc53d7
Torben <git@letorbi.com>
2024-07-08 15:36:39 +0200

    Remove REDS relics from HttpError code

--- HttpError.js
+++ HttpError.js
@@ -1,22 +1,16 @@
-(function(){
 "use strict";
 
-var HttpError = function(code, message, data, exception) {
+module.exports = exports = function(code, message, data, exception) {
     Error.call(this);
-    this.name = "REDS_HTTP_ERROR";
+    this.name = "TARP_HTTPD_ERROR";
     this.code = code||500;
     this.message = message||"";
     this.data = data||null;
     this.exception = exception||null;
 }
 
-HttpError.prototype = Object.create(Error.prototype);
+exports.prototype = Object.create(Error.prototype);
 
-HttpError.prototype.toString = function() {
+exports.prototype.toString = function() {
     return this.code+" ("+this.message+")";
 }
-
-// NOTE Export when loaded as a CommonJS module, add to global reds object otherwise.
-typeof exports=='object' ? module.exports=exports=HttpError : (self.reds=self.reds||new Object()).HttpError = HttpError;
-
-})();
